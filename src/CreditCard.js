351abede394ffeb351fce95a1e83ebe22ea0d664
unknown <mbarekyasmine2017@gmail.com>
2018-11-20 16:24:18 +0100

    first commit

---
+++ src/CreditCard.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+const CreditCard = ()=>{
+    return <div>
+       
+        <div className="main">
+            <h1 className="creditCardTitle">CREDIT CARD</h1>
+            <img className="creditCardSim" src="https://previews.123rf.com/images/miceking/miceking1603/miceking160300029/53109625-chip-de-carte-de-cr%C3%A9dit-carte-%C3%A0-puce-sim.jpg"></img>
+            <h1 className="creditCardNum">7894 1235 7412 8963</h1>
+            <h1 className="creditCardCode">5222</h1>
+            <h2 className="creditCardValid">VALID THRU</h2>
+            <h1 className="creditCardValidMonthYear">11/50</h1>
+            <img className="creditCardName" src="https://s.olj.me/storage/attachments/753/752372_60693905214_645561_large.jpg"></img>
+            <h1 className="creditCardHolder">CARDHOLDER</h1>
+        </div>
+    </div>
+}
+export default CreditCard
